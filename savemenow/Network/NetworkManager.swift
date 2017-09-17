eb0c781dd870a6fe3e455db4b1cfe996f64ca845
Edmund Mok <edmundmok@outlook.com>
2017-09-16 18:36:09 -0700

    Add Network

---
+++ savemenow/Network/NetworkManager.swift
@@ -0,0 +1,42 @@
+//
+//  NetworkManager.swift
+//  savemenow
+//
+//  Created by Edmund Mok on 9/16/17.
+//  Copyright © 2017 Ming Yi Teo. All rights reserved.
+//
+
+import Foundation
+import ArcGIS
+
+protocol Network {
+
+    func getHazards()
+
+    func addHazard()
+
+    func deleteHazard()
+
+    func getRoute()
+
+}
+
+class NetworkManager: Network {
+
+    func getHazards() {
+        <#code#>
+    }
+
+    func addHazard() {
+        <#code#>
+    }
+
+    func deleteHazard() {
+        <#code#>
+    }
+
+    func getRoute() {
+        <#code#>
+    }
+
+}
