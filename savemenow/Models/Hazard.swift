6f6ccc9b21cf472ecb5463c294183bfb7cc7df4d
Edmund Mok <edmundmok@outlook.com>
2017-09-16 20:40:41 -0700

    Add Hazard model

---
+++ savemenow/Models/Hazard.swift
@@ -0,0 +1,21 @@
+//
+//  Hazard.swift
+//  savemenow
+//
+//  Created by Edmund Mok on 9/16/17.
+//  Copyright © 2017 Ming Yi Teo. All rights reserved.
+//
+
+import Foundation
+
+class Hazard {
+
+    let description: String
+    let type: String
+
+    init(description: String, type: String) {
+        self.description = description
+        self.type = type
+    }
+
+}
